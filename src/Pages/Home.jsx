7287779d3c6abbbea071463ff5a34d827befa0a2
Alwallo <alvgonzalezb@gmail.com>
2023-05-17 12:52:59 -0600

    Creación del componente Header e inicio de la page Home

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../assets/css/Home.css'
+import Header from '../Componentes/Header';
+
+function Home() {
+  return (
+    <div>
+        <Header></Header>
+    </div>
+  )
+}
+
+export default Home;
