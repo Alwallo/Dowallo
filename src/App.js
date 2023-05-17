ecccd860af4f50d692e5ac9a90dd053cdbf7968e
Alwallo <alvgonzalezb@gmail.com>
2023-05-17 10:01:09 -0600

    API Incompleta y borrando comentarios de prueba

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Inicio del proyecto</h1>
-      {/* Probando las ramas */}
-      {/* Segunda prueba */}
-      {/* HAHAHA */}
-      {/* Prueba de rama nuevamente */}
-      {/* Cuarta prueba de las ramas */}
-      {/* De nuevo comentario */}
     </div>
   );
 }
